bbae6a4b41ca922964b0b9d21f036d0fc76db461
Смирнов Алексей <79068122307@yandex.ru>
2021-03-22 10:13:46 +0500

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import Search from "./components/Search/Search";
 import UserInfo from "./components/UserInfo/UserInfo";
 import { userInfoQuery } from "./api/startInfo";
 import { useLazyQuery } from "@apollo/client";
-import { getStartData } from "./api/startInfo";
 
 function App() {
-  const [login, setLogin] = useState<string>("");
-  const [data, setData] = useState<any>("");
+    const [login, setLogin] = useState<string>("");
 
+    const [
+        getDataInfo,
+        { data }
+    ] = useLazyQuery(userInfoQuery);
 
-  const loginChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLogin(event.target.value);
-  };
-  
-  const getData = async (e: React.FormEvent<EventTarget>) => {
-    e.preventDefault();
-    const data = await getStartData(login);
-    setData(data);
-  };
+    const loginChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLogin(event.target.value);
+    }
 
-  return (
-    <div className={"container"}>
-      <Search getData={getData} login={login} loginChange={loginChange} />
-      {data ? (
-        <>
-          <UserInfo imgURL={data.avatar_url} login={data.login} />
-        </>
-      ) : null}
-    </div>
-  );
+    const getData = (e: React.FormEvent<EventTarget>) => {
+        e.preventDefault();
+        getDataInfo({
+            variables: { login },
+        })
+    }
+
+    return (
+      <div className={'container'}>
+          <Search getData={getData} login={login} loginChange={loginChange}/>
+          {data ?
+            <>
+                <UserInfo imgURL={data.user.avatarUrl} login={data.user.login}/>
+            </>
+            :
+            null
+          }
+      </div>
+    )
 }
 
 export default App;
