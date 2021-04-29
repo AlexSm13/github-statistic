2272e5fb2122a9fafe8baad2f3ba8e080ef78923
Alex <aleksandr.kz2000@mail.ru>
2021-04-29 19:48:59 +0500

    final 3.0

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { ApolloClient } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client";
 
 const setAccessToken = (userToken?: string) => {
-  let token = "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";
+  let token = process.env.REACT_APP_KEY;
   if (userToken) {
     token = userToken;
   }
